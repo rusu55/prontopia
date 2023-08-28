63a202eae3eddfe965fc79beb4124e07adc92c24
rusu55 <rusu55@outlook.com>
2023-08-28 10:07:43 -0500

    First Commit

---
+++ components/Nav.jsx
@@ -0,0 +1,68 @@
+'use client'
+
+import Link from 'next/link';
+import Image from 'next/image';
+import {useState, useEffect} from 'react';
+import { signIn, signOut, useSession, getProviders} from 'next-auth/react';
+
+const Nav = () => {
+  const isUserLoogedIn = true;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+         <Link href='/' className='flex gap-2 flex-center'>
+            <Image
+              src='/assets/images/logo.svg'
+              alt='logo'
+              width={30}
+              height={30}
+              className='object-contain'
+            />
+            <p className='logo_text'>Promptopia</p>
+         </Link>
+
+         { /* Desktop Navigation */ }
+         <div className="sm:flex hidden">
+            {isUserLoogedIn ? 
+            ( <div className='flex gap-3 md:gap-5'>
+                <Link href="/create-prompt" className='black_btn'>Create New Post</Link>
+                <button onClick={signOut} className='outline_btn'>Sign Out</button>
+                <Link href='/profile'>
+                  <Image src="/assets/images/logo.svg" width={37} height={37}  className='rounded-full' alt='profile' />
+                </Link>
+            </div>) : (
+               <>
+               {providers &&
+                 Object.values(providers).map((provider) => (
+                   <button
+                     type='button'
+                     key={provider.name}
+                     onClick={() => {
+                       signIn(provider.id);
+                     }}
+                     className='black_btn'>
+                     Sign in
+                   </button>
+                 ))}
+             </>
+           )}
+         </div>
+         {/* Mobile Navigation */}
+         <div className="sm:hidden flex relative">
+              {isUserLoogedIn ? (
+                <div></div>
+              ) : ( <div></div>)}
+         </div>
+    </nav>
+  )
+}
+
+export default Nav
