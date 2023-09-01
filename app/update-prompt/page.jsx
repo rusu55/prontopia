1842331c4cd49dbc3b8d76c56d938070e76e1236
rusu55 <rusu55@yahoo.com>
2023-08-31 21:10:33 -0500

    forth

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import {useEffect, useState} from 'react';
+
+import Form from "@components/Form";
 
 const UpdatePrompt = () => {
+    const searchParams = useSearchParams();
+    const promptId = searchParams.get("id");
+    const router = useRouter();
+
+    const [post, setPost] = useState({ prompt: "", tag: "",});
+    const [submitting, setSubmitting] = useState(false);
+
+    useEffect(() =>{
+            const getPrompsDetails = async () =>{
+                const response =  await fetch(`/api/prompt/${promptId}`);
+                const data = await response.json();
+
+                setPost({
+                    prompt: data.prompt,
+                    tag: data.tag,
+                });
+            }
+
+            if(promptId) getPrompsDetails();
+    }, [promptId]);
+
+    const updatePost = async (e) =>{
+        e.preventDefault();
+        setSubmitting(true);
+
+        if(!promptId) return alert("Missing PromptId!");
+
+        try{
+            const response = await fetch(`/api/prompt/${promptId}`,{
+                    method: "PATCH",
+                    body: JSON.stringify({
+                        prompt: post.prompt,
+                        tag: post.tag,
+                    })
+                 });
+            if(response.ok) router.push("/");
+        }catch(error){
+            console.log(error)
+        } finally{
+            setSubmitting(false);
+        }
+    }
   return (
-    <div> UpdatePrompt</div>
+    <Form 
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting= {submitting}
+        handleSubmit={updatePost}
+    />
   )
 }
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
       setProviders(res);      
     })();
   }, []);
-  console.log(session)
+  
   return (
     <nav className="flex-between w-full mb-16 pt-3">
          <Link href='/' className='flex gap-2 flex-center'>
